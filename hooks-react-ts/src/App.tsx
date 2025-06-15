19fe27bffd1986b793d9cca5de4fd64f9e7f1f04
brunosgs <brunosoges@gmail.com>
2025-06-15 14:54:01 -0300

    build: atualizando dependências; feat: introdução a biblioteca styled components css in js.

--- hooks-react-ts/src/App.tsx
+++ hooks-react-ts/src/App.tsx
@@ -1,9 +1,13 @@
-import { useState } from 'react'
+import { Button } from './components/Button'
 
 export function App() {
-   const [count, setCount] = useState(0)
-
    return (
-    <h1>Hello World</h1>
-  )
+      <>
+         <Button variant="primary"></Button>
+         <Button variant="sencodary"></Button>
+         <Button variant="success"></Button>
+         <Button variant="danger"></Button>
+         <Button></Button>
+      </>
+   )
 }
