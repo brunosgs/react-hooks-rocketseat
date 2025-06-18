97935b1e1eb4ed96246c3276b216a85f55cc8e03
brunosgs <brunosoges@gmail.com>
2025-06-17 21:26:39 -0300

    feat: configurando as rotas com react router dom.

--- hooks-react-ts/src/App.tsx
+++ hooks-react-ts/src/App.tsx
@@ -1,17 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
    return (
       <ThemeProvider theme={defaultTheme}>
-         <Button variant="primary"></Button>
-         <Button variant="sencodary"></Button>
-         <Button variant="success"></Button>
-         <Button variant="danger"></Button>
-         <Button></Button>
-         
+         <BrowserRouter>
+            <Router />
+         </BrowserRouter>
          <GlobalStyle />
       </ThemeProvider>
    )
