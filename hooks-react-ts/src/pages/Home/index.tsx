5db3c680ec887bb4d7b5d730671f2de76077aec7
brunosgs <brunosoges@gmail.com>
2025-06-19 12:29:53 -0300

    home input task
    feat: aprimorando input de nome de projetos com uma sugestão pré-definida e removendo com estilização a propriedade do input que mostra uma seta indicando a lista de sugestões.

--- hooks-react-ts/src/pages/Home/index.tsx
+++ hooks-react-ts/src/pages/Home/index.tsx
@@ -17,12 +17,22 @@ export function Home() {
                <label htmlFor="task">Vou trabalhar em</label>
                <TaskInput
                   id="task"
+                  list="task-suggestions"
                   placeholder="Dê um nome para seu projeto"
                />
+               <datalist id="task-suggestions">
+                  <option value="Projeto 1"></option>
+                  <option value="Projeto 2"></option>
+                  <option value="Projeto 3"></option>
+                  <option value="Teste"></option>
+               </datalist>
                <label htmlFor="minutesAmount">durante</label>
                <MinutesAmountInput
                   id="minutesAmount"
                   type="number"
+                  step={5}
+                  min={5}
+                  max={60}
                   placeholder="00" />
                <span>minutos.</span>
             </FormContainer>
