bbf029b6a2b2f9b6f72f4356825ad4f955899b3f
brunosgs <brunosoges@gmail.com>
2025-06-22 12:22:15 -0300

    validando props
    feat: adicionado validador de props transients.

--- hooks-react-ts/src/App.tsx
+++ hooks-react-ts/src/App.tsx
@@ -1,16 +1,22 @@
-import { ThemeProvider } from 'styled-components'
+import isPropValid from '@emotion/is-prop-valid'
+import { BrowserRouter } from 'react-router-dom'
+import { StyleSheetManager, ThemeProvider } from 'styled-components'
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
-import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
    return (
-      <ThemeProvider theme={defaultTheme}>
-         <BrowserRouter>
-            <Router />
-         </BrowserRouter>
-         <GlobalStyle />
-      </ThemeProvider>
+      <StyleSheetManager enableVendorPrefixes
+         shouldForwardProp={(propName, elementToBeRendered) => {
+            return typeof elementToBeRendered === 'string' ? isPropValid(propName) : true;
+         }}>
+         <ThemeProvider theme={defaultTheme}>
+            <BrowserRouter>
+               <Router />
+            </BrowserRouter>
+            <GlobalStyle />
+         </ThemeProvider>
+      </StyleSheetManager>
    )
 }
